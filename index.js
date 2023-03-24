cb796fcc881e6da5e55d5a0ee08801b31f0faede
TehHitamm <128783501+TehHitamm@users.noreply.github.com>
2023-03-24 14:15:48 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const { Client, Collection, Intents } = require('discord.js');
+const fs = require('fs');
+
+const client = new Client({ 
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES
+  ] 
+});
+
+
+client.commands = new Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+for (const folder of commandFolders) {
+  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.data.name, command);
+  }
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.DISCORD_TOKEN);
